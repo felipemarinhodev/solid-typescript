02c17f8d0c72a371319435f9ce7df41818f24568
Felipe Marinho <github@felipemarinho.dev>
2024-03-07 08:37:26 -0800

    a5.3 - Implementando uma nova forma de contrato

--- tarefa-5/controller/ClienteController.ts
+++ tarefa-5/controller/ClienteController.ts
@@ -1,11 +1,8 @@
 import Cliente from "../entities/Cliente";
-import InMemoryRepository from "../repositories/InMemoryRepository";
+import IClienteRepository from "../repositories/interfaces/IClienteRepository";
 
 export default class ClienteController {
-  repository: InMemoryRepository;
-  constructor() {
-    this.repository = new InMemoryRepository();
-  }
+  constructor(private repository: IClienteRepository) {}
 
   adicionaCliente(cliente: Cliente) {
     this.repository.adicionaCliente(cliente);
